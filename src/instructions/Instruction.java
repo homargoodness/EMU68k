8b858c4c0020ef5773ce0660307d7b01397eb931
homargoodness <o.manir@gmail.com>
2013-08-08 18:47:29 +0100

    tidied up move class

--- src/instructions/Instruction.java
+++ src/instructions/Instruction.java
@@ -4,38 +4,39 @@ import architecture.Chip;
 
 public abstract class Instruction {
 	
-	final int SIZE_BYTE = 1;
-	final int SIZE_LONG_WORD = 2;
-	final int SIZE_WORD = 3;
+	protected final int SIZE_BYTE = 1;
+	protected final int SIZE_LONG_WORD = 2;
+	protected final int SIZE_WORD = 3;
 	
-	final int DATA_REG_DIRECT = 0;
-	final int ADDRESS_REG_DIRECT = 1;
-	final int ADDRESS_REG_INDIRECT = 2;
-	final int ADDRESS_REG_INDIRECT_W_POSTINC = 3;
-	final int ADDRESS_REG_INDIRECT_W_PREDEC = 4;
-	final int ADDRESS_REG_INDIRECT_W_DISP = 5;
-	final int ADDRESS_REG_INDIRECT_W_INDEX_8BIT_DISP = 6;
-	final int ADDRESS_REG_INDIRECT_W_INDEX_BASE_DISP = 6;
-	final int MEMORY_INDIRECT_POSTINDEXED = 6;
-	final int MEMORY_INDIRECT_PREINDEXED = 6;
 	
-	final int PC_INDIRECT_W_DISP_MODE_FIELD = 7;
-	final int PC_INDIRECT_W_DISP_REG_FIELD = 2;
+	protected final int DATA_REG_DIRECT = 0;
+	protected final int ADDRESS_REG_DIRECT = 1;
+	protected final int ADDRESS_REG_INDIRECT = 2;
+	protected final int ADDRESS_REG_INDIRECT_W_POSTINC = 3;
+	protected final int ADDRESS_REG_INDIRECT_W_PREDEC = 4;
+	protected final int ADDRESS_REG_INDIRECT_W_DISP = 5;
+	protected final int ADDRESS_REG_INDIRECT_W_INDEX_8BIT_DISP = 6;
+	protected final int ADDRESS_REG_INDIRECT_W_INDEX_BASE_DISP = 6;
+	protected final int MEMORY_INDIRECT_POSTINDEXED = 6;
+	protected final int MEMORY_INDIRECT_PREINDEXED = 6;
 	
-	final int PC_INDIRECT_W_INDEX_MODE_FIELD = 7;
-	final int PC_INDIR_W_INDEX_8BIT_DISP_REG_FIELD = 3;
-	final int PC_INDIR_W_INDEX_BASE_DISP_REG_FIED = 3;
+	protected final int PC_INDIRECT_W_DISP_MODE_FIELD = 7;
+	protected final int PC_INDIRECT_W_DISP_REG_FIELD = 2;
 	
-	final int PC_MEMORY_INDIRECT_MODE_FIELD = 7;
-	final int PC_MEM_INDIRECT_POSTINDEXED_REG_FIELD = 3;
-	final int PC_MEM_INDIRECT_PREINDEXED_REG_FIELD = 3;
+	protected final int PC_INDIRECT_W_INDEX_MODE_FIELD = 7;
+	protected final int PC_INDIR_W_INDEX_8BIT_DISP_REG_FIELD = 3;
+	protected final int PC_INDIR_W_INDEX_BASE_DISP_REG_FIED = 3;
 	
-	final int ASBSOLUTE_MODE_FIELD = 7;
-	final int ABSOLUTE__SHORT_REG_FIELD = 0;
-	final int ABSOLUTE_LONG_REG_FIELD = 0;
+	protected final int PC_MEMORY_INDIRECT_MODE_FIELD = 7;
+	protected final int PC_MEM_INDIRECT_POSTINDEXED_REG_FIELD = 3;
+	protected final int PC_MEM_INDIRECT_PREINDEXED_REG_FIELD = 3;
 	
-	final int IMMEADIATE_MODE_FIELD = 7;
-	final int IMMEDIATE_REG_FIELD = 7;
+	protected final int ASBSOLUTE_MODE_FIELD = 7;
+	protected final int ABSOLUTE__SHORT_REG_FIELD = 0;
+	protected final int ABSOLUTE_LONG_REG_FIELD = 0;
+	
+	protected final int IMMEDIATE_MODE_FIELD = 7;
+	protected final int IMMEDIATE_REG_FIELD = 4;
 	
 	
 	
--- src/instructions/movement/Move.java
+++ src/instructions/movement/Move.java
@@ -9,24 +9,6 @@ import instructions.Instruction;
  */
 public class Move extends Instruction {
 
-	//source mode 0 = data reg
-	//source mode 1 = address
-	//source mode 2 = memory
-	//source mode 3 = memory with postinc
-	//source mode 4 = memory woth predec
-	//source mode 5 = memory with displacement
-	//source mode 6 = memory with index
-	//source mode 7
-	//source 0 = abs short
-	//source 1 = abs long
-	//source 2 = PC with disp
-	//source 3 = PC with index
-	//source 4 = immeadiate
-
-	//01 byte
-	//10 long
-	//11word
-
 	private int opCode; // stores the 16 bit operation code for the instruction
 
 	/**
@@ -37,11 +19,7 @@ public class Move extends Instruction {
 		opCode = aCode;
 	}
 
-
-	/**
-	 * Inherited method which executes the MOVE instruction.
-	 * @param model the the model which is changed by this instruction.
-	 */
+	@Override
 	public void execute(Chip model) {
 
 		int size = (opCode >>> 0xC) & 0x3; // calculate the size parameter of the instruction (b, w, or l)
@@ -52,89 +30,89 @@ public class Move extends Instruction {
 		int operand = 0; // the value being moved
 		
 		// get the operand from the appropriate source
-		if (sourceMode == 0) { // data register direct
-			if (size == 1) { //byte
+		if (sourceMode == DATA_REG_DIRECT) { // data register direct
+			if (size == SIZE_BYTE) { //byte
 				operand = model.getDataRegisterByte(source);
 			}
-			else if (size == 2) { //long word
+			else if (size == SIZE_LONG_WORD) { //long word
 				operand = model.getDataRegisterLongWord(source);
 			}
-			else if (size == 3) { // word
+			else if (size == SIZE_WORD) { // word
 				operand = model.getDataRegisterWord(source);
 			}
 		}
-		else if (sourceMode == 1) { // address register direct
-			if (size == 2) { //long word
+		else if (sourceMode == ADDRESS_REG_DIRECT) { // address register direct
+			if (size == SIZE_LONG_WORD) { //long word
 				operand = model.getAddressRegisterLongWord(source);
 			}
-			else if (size == 3) { // word
+			else if (size == SIZE_WORD) { // word
 				operand = model.getAddressRegisterWord(source);
 			}
 		}
-		else if (sourceMode == 2) { // address register indirect
-			if (size == 1) { //byte
+		else if (sourceMode == ADDRESS_REG_INDIRECT) { // address register indirect
+			if (size == SIZE_BYTE) { //byte
 				operand = model.readMemoryByte(model.getAddressRegisterLongWord(source) & 0xFF);
 			}
-			else if (size == 2) { //long word
+			else if (size == SIZE_LONG_WORD) { //long word
 				operand = model.readMemoryLongWord(model.getAddressRegisterLongWord(source));
 			}
-			else if (size == 3) { // word
+			else if (size == SIZE_WORD) { // word
 				operand = model.readMemoryWord(model.getAddressRegisterLongWord(source) & 0xFFFF);
 			}
 		}
-		else if (sourceMode == 3) { // address register indirect with postincrement
-			if (size == 1) { // byte
+		else if (sourceMode == ADDRESS_REG_INDIRECT_W_POSTINC) { // address register indirect with postincrement
+			if (size == SIZE_BYTE) { // byte
 				int contents = model.getAddressRegisterLongWord(source);
 				model.setAddressRegister(source, contents + 1);
 				operand = model.readMemoryByte(contents);
 			}
-			else if (size == 2) { // long word
+			else if (size == SIZE_LONG_WORD) { // long word
 				int contents = model.getAddressRegisterLongWord(source);
 				model.setAddressRegister(source, contents + 4);
 				operand = model.readMemoryLongWord(contents);
 			}
-			else if (size == 3) { // word
+			else if (size == SIZE_WORD) { // word
 				int contents = model.getAddressRegisterLongWord(source);
 				model.setAddressRegister(source, contents + 2);
 				operand = model.readMemoryWord(contents);
 			}
 		}
-		else if (sourceMode == 4) { // address register indirect with predecrement
-			if (size == 1) { // byte
+		else if (sourceMode == ADDRESS_REG_INDIRECT_W_PREDEC) { // address register indirect with predecrement
+			if (size == SIZE_BYTE) { // byte
 				int contents = model.getAddressRegisterLongWord(source);
 				contents -= 1;
 				model.setAddressRegister(source, contents);
 				operand = model.readMemoryByte(contents);
 			}
-			else if (size == 2) { // long word
+			else if (size == SIZE_LONG_WORD) { // long word
 				int contents = model.getAddressRegisterLongWord(source);
 				contents -= 4;
 				model.setAddressRegister(source, contents);
 				operand = model.readMemoryLongWord(contents);
 			}
-			else if (size == 3) { // word
+			else if (size == SIZE_WORD) { // word
 				int contents = model.getAddressRegisterLongWord(source);
 				contents -= 2;
 				model.setAddressRegister(source, contents);
 				operand = model.readMemoryWord(contents);
 			}
 		}
-		else if (sourceMode == 5) { // address register indirect with displacement
-			if (size == 1) { // byte
+		else if (sourceMode == ADDRESS_REG_INDIRECT_W_DISP) { // address register indirect with displacement
+			if (size == SIZE_BYTE) { // byte
 				int contents = model.getAddressRegisterLongWord(source);
 				int disp = model.readMemoryWord(model.getPC());
 				model.setPC(model.getPC() + 2); // update PC
 				contents += disp;
 				operand = model.readMemoryByte(contents);
 			}
-			if (size == 2) { // long word
+			if (size == SIZE_LONG_WORD) { // long word
 				int contents = model.getAddressRegisterLongWord(source);
 				int disp = model.readMemoryWord(model.getPC());
 				model.setPC(model.getPC() + 2); // update PC
 				contents += disp;
 				operand = model.readMemoryLongWord(contents);
 			}
-			if (size == 3) { // word
+			if (size == SIZE_WORD) { // word
 				int contents = model.getAddressRegisterLongWord(source);
 				int disp = model.readMemoryWord(model.getPC());
 				model.setPC(model.getPC() + 2); // update PC
@@ -142,30 +120,30 @@ public class Move extends Instruction {
 				operand = model.readMemoryWord(contents);
 			}
 		}
-		else if (sourceMode == 6) { // address register indirect with index 
-			if (size == 1) { // byte
+		else if (sourceMode == ADDRESS_REG_INDIRECT_W_INDEX_8BIT_DISP) { // 6
+			if (size == SIZE_BYTE) { // byte
 				
 			}
-			if (size == 2) { // long word
+			if (size == SIZE_LONG_WORD) { // long word
 				
 			}
-			if (size == 3) { // word
+			if (size == SIZE_WORD) { // word
 	
 			}
 		}
-		else if (sourceMode == 7) { 
+		else if (sourceMode == IMMEDIATE_MODE_FIELD) { 
 
-			if (source == 4) { // immediate addressing
+			if (source == IMMEDIATE_REG_FIELD) { // immediate addressing
 
-				if (size == 1) { // byte
+				if (size == SIZE_BYTE) { // byte
 					operand = model.readMemoryByte(model.getPC()) & 0xFF;
 					model.setPC(model.getPC() + 1); // update PC
 				}
-				else if (size == 3) { // word
+				else if (size == SIZE_WORD) { // word
 					operand = model.readMemoryWord(model.getPC());
 					model.setPC(model.getPC() + 2); // update PC
 				}
-				else if (size == 2) { // long word
+				else if (size == SIZE_LONG_WORD) { // long word
 					operand = model.readMemoryLongWord(model.getPC());
 					model.setPC(model.getPC() + 4); // update PC
 				}
@@ -174,92 +152,92 @@ public class Move extends Instruction {
 		
 		
 		// write the operand to the appropriate destination
-		if (destMode == 0) { //data register
-			if (size == 1 ) { // byte
+		if (destMode == DATA_REG_DIRECT) { //data register
+			if (size == SIZE_BYTE ) { // byte
 				model.setDataRegister(dest, (byte)operand);
 			}
-			else if (size == 2) { // long word
+			else if (size == SIZE_LONG_WORD) { // long word
 				model.setDataRegister(dest, operand);
 			}
-			else if (size == 3) { // word
+			else if (size == SIZE_WORD) { // word
 				model.setDataRegister(dest, (short)operand);
 			}
 		}
-		else if (destMode == 1) {// address register
-			if (size == 2) { // long word
+		else if (destMode == ADDRESS_REG_DIRECT) {// address register
+			if (size == SIZE_LONG_WORD) { // long word
 				model.setAddressRegister(dest, operand);
 			}
-			else if (size == 3) { // word
+			else if (size == SIZE_WORD) { // word
 				model.setAddressRegister(dest, (short) operand);
 			}
 		}
-		else if (destMode == 2) { // address register indirect
-			if (size == 1) { // byte
+		else if (destMode == ADDRESS_REG_INDIRECT) { // address register indirect
+			if (size == SIZE_BYTE) { // byte
 				int contents = model.getAddressRegisterLongWord(dest);
 				model.writeMemory(contents, (byte)operand);
 			}
-			else if (size == 2) { // long word
+			else if (size == SIZE_LONG_WORD) { // long word
 				int contents = model.getAddressRegisterLongWord(dest);
 				model.writeMemory(contents, operand);
 			}
-			else if (size == 3) { // word
+			else if (size == SIZE_WORD) { // word
 				int contents = model.getAddressRegisterLongWord(dest);
 				model.writeMemory(contents, (short)operand);
 			}
 		}
-		else if (destMode == 3) { // address register indirect with postincrement
-			if (size == 1) {
+		else if (destMode == ADDRESS_REG_INDIRECT_W_POSTINC) { // address register indirect with postincrement
+			if (size == SIZE_BYTE) {
 				int contents = model.getAddressRegisterLongWord(dest);
 				model.writeMemory(contents, (byte) operand);
 				model.setAddressRegister(dest, contents + 1);
 			}
-			else if (size == 2) {
+			else if (size == SIZE_LONG_WORD) {
 				int contents = model.getAddressRegisterLongWord(dest);
 				model.writeMemory(contents, operand);
 				model.setAddressRegister(dest, contents + 4);
 			}
-			else if (size == 3) {
+			else if (size == SIZE_WORD) {
 				int contents = model.getAddressRegisterLongWord(dest);
 				model.writeMemory(contents, (short) operand);
 				model.setAddressRegister(dest, contents + 2);
 			}
 		}
-		else if (destMode == 4) { // address register indirect with predecrement
-			if (size == 1) {
+		else if (destMode == ADDRESS_REG_INDIRECT_W_PREDEC) { // address register indirect with predecrement
+			if (size == SIZE_BYTE) {
 				int contents = model.getAddressRegisterLongWord(dest);
 				contents -= 1;
 				model.writeMemory(contents, (byte) operand);
 				model.setAddressRegister(dest, contents);
 			}
-			else if (size == 2) {
+			else if (size == SIZE_LONG_WORD) {
 				int contents = model.getAddressRegisterLongWord(dest);
 				contents -= 4;
 				model.writeMemory(contents, operand);
 				model.setAddressRegister(dest, contents);
 			}
-			else if (size == 3) {
+			else if (size == SIZE_WORD) {
 				int contents = model.getAddressRegisterLongWord(dest);
 				contents -= 2;
 				model.writeMemory(contents, (short) operand);
 				model.setAddressRegister(dest, contents);
 			}
 		}
-		else if (destMode == 5) {//TODO test this!!!!!!! disp as source and dest?????????????
-			if (size == 1) { // byte
+		else if (destMode == ADDRESS_REG_INDIRECT_W_DISP) {//TODO test this!!!!!!! disp as source and dest?????????????
+			if (size == SIZE_BYTE) { // byte
 				int contents = model.getAddressRegisterLongWord(dest);
 				int disp = model.readMemoryWord(model.getPC());
 				model.setPC(model.getPC() + 2); // update PC
 				contents += disp;
 				model.writeMemory(contents, (byte)operand);
 			}
-			if (size == 2) { // long word
+			if (size == SIZE_LONG_WORD) { // long word
 				int contents = model.getAddressRegisterLongWord(dest);
 				int disp = model.readMemoryWord(model.getPC());
 				model.setPC(model.getPC() + 2); // update PC
 				contents += disp;
 				model.writeMemory(contents, operand);
 			}
-			if (size == 3) { // word
+			if (size == SIZE_WORD) { // word
 				int contents = model.getAddressRegisterLongWord(dest);
 				int disp = model.readMemoryWord(model.getPC());
 				model.setPC(model.getPC() + 2); // update PC
@@ -267,20 +245,18 @@ public class Move extends Instruction {
 				model.writeMemory(contents, (short)operand);
 			}
 		}
-		else if (destMode == 6) {
-			if (size == 1) { // byte
+		else if (destMode == ADDRESS_REG_INDIRECT_W_INDEX_8BIT_DISP) {
+			if (size == SIZE_BYTE) { // byte
 				
 			}
-			if (size == 2) { // long word
+			if (size == SIZE_LONG_WORD) { // long word
 				
 			}
-			if (size == 3) { // word
+			if (size == SIZE_WORD) { // word
 	
 			}
 		}
-		else if (destMode == 7) {
-			
-		}
+		
 		
 		
 		// set SR flags
