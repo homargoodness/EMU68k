6e5a677362356633f52a67d030b36b6b6b6caaad
homargoodness <o.manir@gmail.com>
2013-08-18 23:10:32 +0100

    added comments to memory and chip classes

--- src/architecture/Chip.java
+++ src/architecture/Chip.java
@@ -4,83 +4,217 @@ import java.beans.PropertyChangeListener;
 
 
 /**
- *
+ * Interface for the mediator aggregate class which holds references to the registers and
+ * memory and contains the logic which controls access to them. This is the class which
+ * instructions use to interact with the model
  */
 public interface Chip {
 	
+	/**
+	 * Method to add a listener to this class which gets updated when the model changes
+	 * @param listener the listener which is to be notified of change
+	 */
 	public void addListener(PropertyChangeListener listener);
 	
+	/**
+	 * Method to reset all registers and memory to their initial state
+	 */
 	public void reset();
 	
-	
-	/** Memory **/
+	/**
+	 * Method to write a single byte to memory
+	 * @param address the address where the data is to written
+	 * @param data the byte of data to be written to memory
+	 */
 	public void writeMemoryByte(int address, int data);
 	
+	/**
+	 * Method to write a word (2 bytes) to memory. The least significant byte
+	 * is placed in the lower numbered memory address
+	 * @param address the start address where the data is to be written
+	 * @param data the word of data to be written to memory
+	 */
 	public void writeMemoryWord(int address, int data);
 	
+	/**
+	 * Method to write a long word (4 bytes) to memory. The least significant byte
+	 * is placed in the lower numbered memory address
+	 * @param address the start address where the data is to be written
+	 * @param data the long word of data to be written to memory
+	 */
 	public void writeMemoryLongWord(int address, int data);
 	
+	/**
+	 * Method to read a byte from memory 
+	 * @param address the address where the byte resides
+	 * @return the byte of data read from memory
+	 */
 	public int readMemoryByte(int address);
 	
+	/**
+	 * Method to read a word (2 bytes) from memory starting at a given address. The least significant
+	 * byte is located in the numerically lowest address space
+	 * @param address the starting address of the word which holds the low order byte
+	 * @return the word read from memory
+	 */
 	public int readMemoryWord(int address);
 	
+	/**
+	 * Method to a long word (4 bytes) from memory starting at a given address. The least
+	 * significant byte is located in the starting address
+	 * @param address the location of the least significant byte and the start of the long word
+	 * @return the long word from memory
+	 */
 	public int readMemoryLongWord(int address);
 	
-	
-	/** PC **/
+	/**
+	 * Method to set the value of the Program Counter
+	 * @param address the value to set the PC to
+	 */
 	public void setPC(int address);
 	
+	/**
+	 * Method to return the contents of the PC
+	 * @return the contents of the PC
+	 */
 	public int getPC();
 	
-	
-	/** Data Register **/
+	/**
+	 * Method to write to the low order byte of the specified data register
+	 * @param reg the data register to be modified
+	 * @param data the byte to be written to the register
+	 */
 	public void setDataRegisterByte(int reg, int data);
 	
+	/**
+	 * Method to write to the low order word of the specified data register
+	 * @param reg data the register to be written to
+	 * @param data the word to be written
+	 */
 	public void setDataRegisterWord(int reg, int data);
 	
+	/**
+	 * Method to write a long word to the specified data register
+	 * @param reg the register to write to
+	 * @param data the long word to be written
+	 */
 	public void setDataRegisterLongWord(int reg, int data);
 	
-	
+	/**
+	 * Method to return the low order byte contained in the specified data register
+	 * @param reg the data register to read from
+	 * @return the low order byte held in the data register
+	 */
 	public int getDataRegisterByte(int reg);
 	
+	/**
+	 * Method to return the low order word held in the specified data register
+	 * @param reg the data register to read from
+	 * @return the low order word held in the register
+	 */
 	public int getDataRegisterWord(int reg);
 	
+	/**
+	 * Method to return the long word held in the specified data register
+	 * @param reg the data register to read from
+	 * @return the long word held in the register
+	 */
 	public int getDataRegisterLongWord(int reg);
 	
-	
-	/** Address Register **/
+	/**
+	 * Method to write to the low order word in the specified address register
+	 * @param reg the address register to write to
+	 * @param data the word to write to the address register
+	 */
 	public void setAddressRegisterWord(int reg, int data);
 	
+	/**
+	 * Method to write a long word to a specified address register
+	 * @param reg the register to write to
+	 * @param data the long word to write to register
+	 */
 	public void setAddressRegisterLongWord(int reg, int data);
 	
-	
+	/**
+	 * Method to return the low order word from the specified address register
+	 * @param reg the register to read from
+	 * @return the low order word held in the register
+	 */
 	public int getAddressRegisterWord(int reg);
 	
+	/**
+	 * Method to return the long word held in the specified address register
+	 * @param reg the register to read from
+	 * @return the long word held in the register
+	 */
 	public int getAddressRegisterLongWord(int reg);
 	
-	
-	/** SR **/
+	/**
+	 * Method to return the value of the first bit(Carry bit) held in the register
+	 * @return the carry bit flag value
+	 */
 	public int getSRCarryBit();
 	
+	/**
+	 * Method to return the value of the second bit position (overflow bit) in the 
+	 * status register
+	 * @return the value of the overflow flag
+	 */
 	public int getSROverflowBit();
 	
+	/**
+	 * Method to return the value of the third bit position (zero bit) in the 
+	 * status register
+	 * @return the value of the zero flag
+	 */
 	public int getSRZeroBit();
 	
+	/**
+	 * Method to return the value of the fourth bit position (negative bit) in the 
+	 * status register
+	 * @return the value of the negative flag
+	 */
 	public int getSRNegativeBit();
 	
+	/**
+	 * Method to return the value of the fifth bit position (extend bit) in the 
+	 * status register
+	 * @return the value of the extend flag
+	 */
 	public int getSRExtendBit();
 	
-	
+	/**
+	 * Method to set the value of the first bit(Carry bit) in the register
+	 * @param the new value of the carry flag
+	 */
 	public void setSRCarryBit(int bit);
 	
+	/**
+	 * Method to set the value of the second bit position (overflow bit) in the 
+	 * status register
+	 * @param the new value of the overflow flag
+	 */
 	public void setSROverflowBit(int bit);
 	
+	/**
+	 * Method to set the value of the third bit position (zero bit) in the 
+	 * status register
+	 * @param the new value of the zero flag
+	 */
 	public void setSRZeroBit(int bit);
 	
+	/**
+	 * Method to set the value of the fourth bit position (negative bit) in the 
+	 * status register
+	 * @param bit the new value for the negative flag
+	 */
 	public void setSRNegativeBit(int bit);
 	
+	/**
+	 * Method to set the value of the fifth bit position (extend bit) in the 
+	 * status register
+	 * @param bit the new value of the extend flag
+	 */
 	public void setSRExtendBit(int bit);
-	
-	
 
 }
--- src/architecture/memory/Memory.java
+++ src/architecture/memory/Memory.java
@@ -4,68 +4,67 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-
-
 /**
- * Emulates the memory system of the MC68000 Each memory location holds 1 byte
- * (8 bits) which has a value between 0 and 255 Address bus is 24 bits wide so
- * locations 000000 to FFFFFF (hex) are addressable Memory is address using
- * hexadecimal
+ * Data structure which emulates the 68k memory space with method to set and retrieve the data
+ * at a particular memory location.
  * 
- * @author Omar Manir
+ * Each memory location is represented by the MemoryLocation inner class which holds the address
+ * and memory contents
  * 
- * 
- *
+ * The contents are held in an arraylist. When a new valueis written to memory, the list is checked
+ * to see if there is already a value in the memory location, if there is it is overwritten, otherwise
+ * a new element is added to the list. When reading from memory, first the list is checked, if the address is 
+ * populated, the contents are returned otherwise a default value of 0xFF is returned.
  */
 public class Memory {
 	
-	private List<MemoryLocation> memory;
+	private List<MemoryLocation> memory; // the list holding the contents of memory
 
-	//private final int SIZE = 0xFFFFFF;
-
+	/**
+	 * Constructor
+	 */
 	public Memory() {
 		memory = new ArrayList<MemoryLocation>();
 	}
 
 	
 	/**
-	 * TODO synchronize
-	 * @param address
-	 * @return
-	 * @throws MemoryAccessException
+	 * Method to read a byte from a specified memory location
+	 * @param address the address to read from
+	 * @return the byte at the memory location
 	 */
 	public byte read(int address) {
 		
-		for (int i = 0; i < memory.size(); i++) {
-			if (memory.get(i).address == address) {
-				return memory.get(i).contents;
+		for (int i = 0; i < memory.size(); i++) { // iterate through all memory locations held in list
+			if (memory.get(i).address == address) { // if the address is found
+				return memory.get(i).contents; // return the byte of data held there
 			}
 		}
-		
-		return (byte)0xFF;
+		return (byte)0xFF; // otherwise return value 0xFF
 	}
 	
 
 	/**
-	 * TODO synchronize
-	 * @param data
-	 * @param address
-	 * @throws MemoryAccessException
-	 * @throws StorageException
+	 * Method to write a byte to memory. If the memory location already exists in the list, the value is overwritten 
+	 * with the new value, otherise a new element is added to list
+	 * @param data the byte to be written to memory
+	 * @param address the location to write the data
 	 */
 	public void write(int address, byte data) {
 		
-		for (int i = 0; i < memory.size(); i++) {
-			if (memory.get(i).address == address) {
-				
-				memory.get(i).contents = data;
-				return;
+		for (int i = 0; i < memory.size(); i++) { // loop through all memeory locations held in list
+			if (memory.get(i).address == address) { // if the address is found
+				memory.get(i).contents = data; // overwrite the contents
+				return; // end method
 			}
 		}
-		
-		memory.add(new MemoryLocation(address, data));
+		memory.add(new MemoryLocation(address, data)); // if the address was not found return 0xFF
 	}
 	
+	/** 
+	 * Method to empty the memory
+	 * TODO need the loop???
+	 */
 	public void reset() {
 		int size = memory.size();
 		for (int i = 0; i < size; i++) {
@@ -74,17 +73,24 @@ public class Memory {
 		memory.clear();
 	}
 	
+	
+	/**
+	 * Inner class which is used to model 1 memory location which has an address
+	 * and contents in the form of a byte
+	 */
 	private class MemoryLocation {
-		private int address;
-		private byte contents;
+		private int address; // the address of this memory location
+		private byte contents; // the data this memory location holds
 		
+		/**
+		 * Constructor
+		 * @param anAddress the address of the new memory location
+		 * @param data the data to be written to the memory location
+		 */
 		private MemoryLocation(int anAddress, byte data) {
 			address = anAddress;
 			contents = data;
 		}
 	}
 	
-	
-	
-
 }
